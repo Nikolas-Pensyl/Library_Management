4e69b510fbb93ccbc282714b7959c0a9cf27f276
Darius Surgenavic <dsurgenavic@gmail.com>
2022-02-01 12:39:25 -0500

    renamed index to be .php fixed home button

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="./Styles/main.css">
+		<link rel="stylesheet" type="text/css" href="./Styles/nav.css">
+	</head>
+	<body>
+	<div id="navInd">
+
+	
+	
+	
+	  	<header>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingnavbar">
+            		<span class="navbar-toggler-icon"></span>
+        		</button>
+        		<div class="navbar-collapse collapse" id="collapsingnavbar">
+					<ul class="navbar-nav nav-fill">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#About">Edit Members</a></li>
+						<li><a href="#Sponsors">Partners</a></li>
+						<li><a href="index.php">Borrow Books</a></li>
+						<li><a href="index.php">Search</a></li>
+						<li><a href="index.php">Login</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	
+
+
+
+
+
+</div>
+	</body>	
+</html>
