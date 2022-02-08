ea9252858568b02aa580ad827bc6046752705446
ekdickerson17 <79267433+ekdickerson17@users.noreply.github.com>
2022-02-08 12:00:01 -0500

    Add files via upload

--- CreateTableManagers.php
+++ CreateTableManagers.php
@@ -2,7 +2,7 @@
 	include('config.php');
 
 	// sql to create table
-	$sql = "CREATE TABLE IF NOT EXISTS Members (
+	$sql = "CREATE TABLE IF NOT EXISTS Managers (
 	Name varchar(20) NOT NULL,
 	U_Card int NOT NULL,
 	Address varchar(30) NOT NULL,
@@ -12,7 +12,7 @@
 
 
 	if (mysqli_query($conn, $sql)) {
-		echo "Table Members created successfully";
+		echo "Table Managers created successfully";
 	} else {
 		echo "Error creating table: " . mysqli_error($conn);
 	}
