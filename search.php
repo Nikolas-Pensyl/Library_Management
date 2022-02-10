627e232e10ca7abe784c8190af70faa3def20126
Darius Surgenavic <dsurgenavic@gmail.com>
2022-02-10 13:04:20 -0500

    added search page

---
+++ search.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Search</title>
+		<link rel = stylesheet href = Styles/body.css type = text/css>
+		<link rel="stylesheet" type="text/css" href="./Styles/main.css">
+		<link rel="stylesheet" type="text/css" href="./Styles/nav.css">
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingnavbar">
+            		<span class="navbar-toggler-icon"></span>
+        		</button>
+        		<div class="navbar-collapse collapse" id="collapsingnavbar">
+					<ul class="navbar-nav nav-fill">
+						<li><a href="main.php">Home</a></li>
+						<li><a href="#About">Edit Members</a></li>
+						<li><a href="#Sponsors">Partners</a></li>
+						<li><a href="main.php">Borrow Books</a></li>
+						<li><a href="search.html">Search</a></li>
+						<li><a href="main.php">Login</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+		<div>
+			<div class = mainbody>
+				<div>
+					<form >
+						<input type="text">
+						<input type="submit" value="Search">
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
